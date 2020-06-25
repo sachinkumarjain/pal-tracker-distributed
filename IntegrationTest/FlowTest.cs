975ecaa9257f7063a740c8325656020c58d6a461
unknown <sachinkumarjain@outlook.com>
2020-06-25 10:36:58 +0000

    Secure API with SSO

--- IntegrationTest/FlowTest.cs
+++ IntegrationTest/FlowTest.cs
@@ -25,6 +25,7 @@ namespace IntegrationTest
                 .Port(8883)
                 .Database("tracker_registration_dotnet_test")
                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDREGISTERWITHEUREKA", "false")
+                .SetEnvironmentVariable("DISABLE_AUTH", "true")
                 .Build();
 
             _allocationsServer = TestAppServerBuilder()
@@ -41,6 +42,7 @@ namespace IntegrationTest
                 .Database("tracker_backlog_dotnet_test")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
+                .SetEnvironmentVariable("DISABLE_AUTH", "true")
                 .Build();
 
             _timesheetsServer = TestAppServerBuilder()
@@ -49,6 +51,7 @@ namespace IntegrationTest
                 .Database("tracker_timesheets_dotnet_test")
                 .SetEnvironmentVariable("REGISTRATION_SERVER_ENDPOINT", _registrationServer.Url())
                 .SetEnvironmentVariable("EUREKA__CLIENT__SHOULDFETCHREGISTRY", "false")
+                .SetEnvironmentVariable("DISABLE_AUTH", "true")
                 .Build();
         }
 
