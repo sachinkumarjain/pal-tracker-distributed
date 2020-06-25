f66da729471e86dfc2ce36d0ae66c14f8fbc03df
unknown <sachinkumarjain@outlook.com>
2020-06-25 07:21:40 +0000

    CircuitBreaker

--- Applications/AllocationsServer/Startup.cs
+++ Applications/AllocationsServer/Startup.cs
@@ -11,6 +11,8 @@ using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 using Steeltoe.Management.CloudFoundry;
 using Steeltoe.Discovery.Client;
 using Steeltoe.Common.Discovery;
+using Microsoft.Extensions.Logging;
+using Steeltoe.CircuitBreaker.Hystrix;
 
 namespace AllocationsServer
 {
@@ -42,8 +44,12 @@ namespace AllocationsServer
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
 
-                return new ProjectClient(httpClient);
+                var logger = sp.GetService<ILogger<ProjectClient>>();
+                 return new ProjectClient(httpClient, logger);
+
             });
+
+            services.AddHystrixMetricsStream(Configuration);
             
         }
 
@@ -67,6 +73,8 @@ namespace AllocationsServer
             });
 
             app.UseDiscoveryClient();
+             app.UseHystrixMetricsStream();
+             app.UseHystrixRequestContext();
         }
     }
 }
